784852077d91e0128b7ce3fff08db4264c60a6a5
esta1d <skillet_skillet98@mail.ru>
2025-06-08 07:20:05 +0300

    tera online

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # script_for_tera_online
-Skill prediction for online game tera online
+Skill prediction for online game tera online. Ping emulation in 0, since there is no built-in emulator in the game
---
+++ skill prediction for tera online/fast-script/skills.js
@@ -0,0 +1,97 @@
+// арч
+module.exports.archer = [
+{ "id": 5, "delay": 0, "speed": 15 }, // roa
+{ "id": 8, "delay": 0, "speed": 15 }, // rf
+{ "id": 22, "delay": 0, "speed": 0 }, // sf
+{ "id": 29, "delay": 0, "speed": 5 }, // tb
+{ "id": 3, "delay": 0, "speed": 5 }, // ra
+{ "id": 4, "delay": 0, "speed": 5 } // pa
+
+];
+
+// спинер
+module.exports.assassin = [
+];
+
+// зерк
+module.exports.berserker = [
+];
+
+// мист
+module.exports.elementalist = [
+];
+
+// ганер
+module.exports.engineer = [
+];
+
+// бравлер
+module.exports.fighter = [
+];
+
+// валк
+module.exports.glaiver = [
+];
+
+// рыц
+module.exports.lancer = [
+{ "id": 2, "delay": 0, "speed": 0 }, // Stand fast
+{ "id": 3, "delay": 0, "speed": 10 }, // Onslaught
+{ "id": 4, "delay": 0, "speed": 10 }, // shout
+{ "id": 1, "delay": 0, "speed": 10 }, // autoattack
+{ "id": 5, "delay": 0, "speed": 10 }, // bash
+{ "id": 7, "delay": 0, "speed": 10 }, // gs
+{ "id": 8, "delay": 0, "speed": 5 }, // shield counter
+{ "id": 9, "delay": 0, "speed": 5 }, // leash
+{ "id": 10, "delay": 0, "speed": 5 }, // debil
+{ "id": 11, "delay": 0, "speed": 5 }, // retal
+{ "id": 13, "delay": 0, "speed": 10 }, // spring
+{ "id": 15, "delay": 0, "speed": 10 }, // probejka
+{ "id": 16, "delay": 0, "speed": 15 }, // second wind
+{ "id": 17, "delay": 0, "speed": 0 }, // a-rush
+{ "id": 18, "delay": 0, "speed": 5 }, // barrage
+{ "id": 28, "delay": 0, "speed": 5 }, // leap
+{ "id": 25, "delay": 0, "speed": 5 }, // wallop
+{ "id": 21, "delay": 0, "speed": 10 } // lockdown
+];
+
+// прист
+module.exports.priest = [
+];
+
+// слойка
+module.exports.slayer = [
+];
+
+// сорк
+module.exports.sorcerer = [
+];
+
+// рипак
+module.exports.soulless = [
+];
+
+// вар
+module.exports.warrior = [
+	{ "id": 3, "delay": 0, "speed": 30 }, // Torrent of Blows
+	{ "id": 4, "delay": 0, "speed": 30 }, // Rain of Blows
+	{ "id": 11, "delay": 0, "speed": 30 }, // Poison Blade
+	{ "id": 12, "delay": 0, "speed": 30 }, // Leaping Strike
+	{ "id": 16, "delay": 0, "speed": 30 }, // Charging Slash
+	{ "id": 17, "delay": 0, "speed": 30 }, // Vortex Slash
+	{ "id": 18, "delay": 0, "speed": 30 }, // Combative Strike
+	{ "id": 19, "delay": 0, "speed": 30 }, // Rising Fury
+	{ "id": 21, "delay": 0, "speed": 30 }, // Cascade of Stuns
+	{ "id": 22, "delay": 0, "speed": 30 }, // Backstab
+	{ "id": 28, "delay": 0, "speed": 10 }, // Traverse Cut
+	{ "id": 29, "delay": 0, "speed": 30 }, // Blade Draw
+	{ "id": 30, "delay": 0, "speed": 30 }, // Scythe
+	{ "id": 31, "delay": 0, "speed": 30 }, // Reaping Slash
+	{ "id": 36, "delay": 0, "speed": 30 }, // Rain of Blows (Deadly Gamble)
+	{ "id": 37, "delay": 0, "speed": 30 }, // Blade Draw (Deadly Gamble)
+	{ "id": 38, "delay": 0, "speed": 30 }, // Scythe (Deadly Gamble)
+	{ "id": 39, "delay": 0, "speed": 10 }, // Traverse Cut (Defensive Stance)
+	{ "id": 40, "delay": 0, "speed": 30 }, // Blade Waltz
+	{ "id": 41, "delay": 0, "speed": 30 }, // Aerial Scythe
+	{ "id": 42, "delay": 0, "speed": 30 }, // Blade Frenzy
+];
